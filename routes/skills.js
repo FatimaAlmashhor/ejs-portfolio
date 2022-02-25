76e0a477191e1677a33e2ab121914c1ef9396410
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-02-25 09:26:19 +0300

    Init tailwind

--- routes/skills.js
+++ routes/skills.js
@@ -1,5 +1,5 @@
 const route = require('express').Router()
-const skills = require('../schemas/skills')
+const Skills = require('../schemas/skills')
 
 route.get('/', (req, res) => {
     res.render('dashboard', {
@@ -8,8 +8,27 @@ route.get('/', (req, res) => {
     console.log('here the skills');
 });
 
-route.post('/' , async (req , res )=> {
-    
+route.post('/', async (req, res) => {
+    try {
+        console.log(req.body);
+        const skillsInstanse = new Skills({
+            skill_name: req.body.skill,
+            skill_type: req.body.typeOfskill,
+            is_active: true
+        })
+
+        skillsInstanse.save((err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log(result);
+            }
+        })
+        res.end()
+    } catch (error) {
+        console.log({ error });
+    }
 })
 
 module.exports = route
