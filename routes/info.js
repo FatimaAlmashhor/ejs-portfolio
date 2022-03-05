ee92a7029d3c8801dcd5471cbf4fe53f700a0043
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-03-06 00:55:20 +0300

    Feat : social

--- routes/info.js
+++ routes/info.js
@@ -4,17 +4,21 @@ const ejs = require('ejs')
 const verify = require('../middlewares/verifyToken')
 const removeFile = require('../utils/js/removeFile')
 const upload = require('../utils/js/multer')
-const { restart } = require('nodemon')
+const Activities = require('../schemas/activities')
+const Social = require('../schemas/socials')
+const mongoose = require('mongoose')
 var all = [];
 
 route.get('/', verify, async (req, res) => {
     var info = await Info.find({ deleted: false }).clone().catch(function (err) { console.log(err) });
+    var social = await Social.find({ deleted: false }).clone().catch(function (err) { console.log(err) });
     all = info;
     let user = req.cookies.auth;
     res.render('dashboard', {
         currentPage: 'info',
         data: all,
-        formInfo:{},
+        social: social,
+        formInfo: {},
         userInfo: { name: user.fullname, role: user.auth_role }
     })
     console.log('here the info');
@@ -36,6 +40,15 @@ route.post('/', verify, upload.single('cv'), async (req, res) => {
                 return res.redirect('/500page');
             }
             else {
+                Activities({
+                    table_name: 'infos',
+                    row_id: mongoose.Types.ObjectId(result.id),
+                    auth_id: mongoose.Types.ObjectId(req.cookies.auth._id),
+                    actions_type: 1,
+                    actions_time: new Date()
+                }).save((err, result) => {
+                    if (err) return res.redirect('/500page');
+                })
                 all.push(result)
                 res.redirect('/dashboard/info')
                 res.end()
@@ -48,20 +61,31 @@ route.post('/', verify, upload.single('cv'), async (req, res) => {
 });
 route.get('/delete/:id', async (req, res) => {
     try {
-        await Info.updateOne({
+
+
+        await Info.findByIdAndUpdate({
             _id: req.params.id.replace(/ /g, "")
         },
             {
                 deleted: true,
-            },
-            (error, result) => {
-                if (error) console.log({ error });
-                else {
-                    res.redirect('/dashboard/info')
-                    res.end()
-                }
-            }).clone()
+            }).exec()
+            .then(result => {
+                Activities({
+                    table_name: 'infos',
+                    row_id: mongoose.Types.ObjectId(result.id),
+                    auth_id: mongoose.Types.ObjectId(req.cookies.auth._id),
+                    actions_type: 3,
+                    actions_time: new Date()
+                }).save((err, result) => {
+                    if (err) return res.redirect('/500page');
+                })
 
+                res.redirect('/dashboard/info')
+                res.end()
+
+            }).catch(error => {
+                console.log({ error });
+            })
 
     } catch (error) {
         console.log({ error });
@@ -69,13 +93,29 @@ route.get('/delete/:id', async (req, res) => {
 });
 route.get('/toggle/:id/', async (req, res) => {
     try {
-        const newLocal = req.query.state ? false : true
-        console.log(newLocal);
-        await Info.updateOne({
+        const filter = {
             _id: req.params.id.replace(/ /g, "")
-        },
+        }
+        let doc = await Info.findOne(filter, (error, result) => {
+            if (error) return res.redirect('500page');
+            else {
+                Activities({
+                    table_name: 'infos',
+                    row_id: mongoose.Types.ObjectId(result.id),
+                    auth_id: mongoose.Types.ObjectId(req.cookies.auth._id),
+                    actions_type: 2,
+                    actions_time: new Date()
+                }).save((err, result) => {
+                    if (err) return res.redirect('/500page');
+                })
+            }
+        }).clone();
+        const newLocal = doc.is_active ? false : true
+        console.log(newLocal);
+
+        await Info.updateOne(filter,
             {
-                is_active: (newLocal)
+                is_active: newLocal
             },
             (error, result) => {
                 if (error) console.log({ error });
@@ -85,7 +125,6 @@ route.get('/toggle/:id/', async (req, res) => {
                     res.end()
                 }
             }).clone()
-
     } catch (error) {
         console.log({ error });
     }
@@ -129,17 +168,24 @@ route.post('/edit', verify, async (req, res) => {
                 bio: req.body.bio,
                 about: req.body.about,
                 // cv: req.file !== undefined ? req.file.filename : null,
-            },
-            (error, result) => {
-                if (error) {
-                    console.log({ error });
-                    res.redirect('/500page')
-                }
-                else {
-                    res.redirect('/dashboard/info')
-                    res.end()
-                }
-            }).clone()
+            }).exec()
+            .then(result => {
+                Activities({
+                    table_name: 'infos',
+                    row_id: mongoose.Types.ObjectId(result.id),
+                    auth_id: mongoose.Types.ObjectId(req.cookies.auth._id),
+                    actions_type: 2,
+                    actions_time: new Date()
+                }).save((err, result) => {
+                    if (err) return res.redirect('/500page');
+                })
+
+                res.redirect('/dashboard/info')
+                res.end()
+
+            }).catch(error => {
+                console.log({ error });
+            })
 
     } catch (error) {
         res.redirect('/500page')
@@ -159,14 +205,24 @@ route.post('/edit/resume', verify, upload.single('cv'), async (req, res) => {
         },
             {
                 cv: req.file !== undefined ? req.file.filename : doc.cv,
-            },
-            (error, result) => {
-                if (error) console.log({ error });
-                else {
-                    res.redirect('/dashboard/info')
-                    res.end()
-                }
-            }).clone()
+            }).exec()
+            .then(result => {
+                Activities({
+                    table_name: 'infos',
+                    row_id: mongoose.Types.ObjectId(result.id),
+                    auth_id: mongoose.Types.ObjectId(req.cookies.auth._id),
+                    actions_type: 2,
+                    actions_time: new Date()
+                }).save((err, result) => {
+                    if (err) return res.redirect('/500page');
+                })
+
+                res.redirect('/dashboard/info')
+                res.end()
+
+            }).catch(error => {
+                console.log({ error });
+            })
 
     } catch (error) {
         console.log({ error });
@@ -236,6 +292,41 @@ route.get('/progress/delete/:id', verify, async (req, res) => {
 });
 
 
+// add social
+route.post('/social', verify, async (req, res) => {
+    try {
+        console.log('[body]', req.body);
+
+        await new Social({
+            social_title: req.body.social_title,
+            social_link: req.body.social_link,
+            social_icon: req.body.social_icon,
+            is_active: true,
+            deleted: false
+        }).save((err, result) => {
+            if (err) {
+                console.log(err);
+                return res.redirect('/500page');
+            }
+            else {
+                Activities({
+                    table_name: 'infos',
+                    row_id: mongoose.Types.ObjectId(result.id),
+                    auth_id: mongoose.Types.ObjectId(req.cookies.auth._id),
+                    actions_type: 1,
+                    actions_time: new Date()
+                }).save((err, result) => {
+                    if (err) return res.redirect('/500page');
+                })
+                all.push(result)
+                res.redirect('/dashboard/info')
+                res.end()
+            }
+        })
+    } catch (error) {
+        console.log({ error });
+    }
+});
 
 
 
--- schemas/socials.js
+++ schemas/socials.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 const social = new mongoose.Schema({
     social_title: String,
     social_link: String,
-    icon :String ,
+    social_icon: String,
     is_active: Boolean,
+    deleted: Boolean
 })
 
 const socialModel = mongoose.model('social', social);
-
+social.index({ social_title: 1, deleted: 1 }, { unique: true })
 module.exports = socialModel
